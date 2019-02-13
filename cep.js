d95f39ae1c1f5618170fcf4dc39ed9a147c39517
Carol <anacssnts@gmail.com>
2019-02-13 19:32:45 -0200

    validar campos

---
+++ cep.js
@@ -0,0 +1,43 @@
+// jquery wrapper
+
+$(function(){
+
+    console.log(`
+    
+    ／l、         Silent Judging Cat
+    (°､ 。７                   is    
+     l、 ~～ヽ           Silently
+     じし _,)√    Judging you....
+
+    
+    `);
+
+    // ações
+    
+    var onlyNumbers = function(e){
+        // console.log(e.target.value);
+        this.value = this.value.replace(/\D/g,""); // esse g habilita modo global de pesquisa, possibilitando a substituição de TODO NÚMERO pra asterísco
+    }
+
+    var validateEntry = function(){
+        // console.log(this.value);
+        var cep = this.value;
+        if(cep.length < 8){
+            $(this).addClass("error").focus();
+        }else{
+            $(this).removeClass("error");
+            getAddress(cep);
+        }
+    }
+
+    var getAddress = function(){
+
+    }
+
+    // eventos
+    
+    $("#cep")
+    .on("input",onlyNumbers)
+    .on("focusout",validateEntry);
+    
+});
